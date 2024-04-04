03e4fdcc82afa3259051d4f610bf5b8bbd9a744e
Jake Osborne <jake@jakeiswritingcode.com>
2024-04-03 22:16:30 -0500

    remove previous default positions

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -2,59 +2,15 @@ import { useState } from 'react';
 import './Chessboard.css';
 import Tile from '../Tile/Tile';
 import Piece from '../Piece/Piece';
+import initialPositions from '../../data/initialPositions';
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["8", "7", "6", "5", "4", "3", "2", "1"];
 
-interface PieceInfo {
-    type: string;
-    color: string;
-}
-
-interface PiecePosition {
-    [key: string]: PieceInfo | undefined;
-}
-
 export default function Chessboard() {
-    const [piecePosition, setPiecePosition] = useState<PiecePosition>({
-        "a1": { color: "White", type: "Rook" },
-        "b1": { color: "White", type: "Knight" },
-        "c1": { color: "White", type: "Bishop" },
-        "d1": { color: "White", type: "Queen" },
-        "e1": { color: "White", type: "King" },
-        "f1": { color: "White", type: "Bishop" },
-        "g1": { color: "White", type: "Knight" },
-        "h1": { color: "White", type: "Rook" },
-
-        "a2": { color: "White", type: "Pawn" },
-        "b2": { color: "White", type: "Pawn" },
-        "c2": { color: "White", type: "Pawn" },
-        "d2": { color: "White", type: "Pawn" },
-        "e2": { color: "White", type: "Pawn" },
-        "f2": { color: "White", type: "Pawn" },
-        "g2": { color: "White", type: "Pawn" },
-        "h2": { color: "White", type: "Pawn" },
-
-        "a7": { color: "Black", type: "Pawn" },
-        "b7": { color: "Black", type: "Pawn" },
-        "c7": { color: "Black", type: "Pawn" },
-        "d7": { color: "Black", type: "Pawn" },
-        "e7": { color: "Black", type: "Pawn" },
-        "f7": { color: "Black", type: "Pawn" },
-        "g7": { color: "Black", type: "Pawn" },
-        "h7": { color: "Black", type: "Pawn" },
-
-        "a8": { color: "Black", type: "Rook" },
-        "b8": { color: "Black", type: "Knight" },
-        "c8": { color: "Black", type: "Bishop" },
-        "d8": { color: "Black", type: "Queen" },
-        "e8": { color: "Black", type: "King" },
-        "f8": { color: "Black", type: "Bishop" },
-        "g8": { color: "Black", type: "Knight" },
-        "h8": { color: "Black", type: "Rook" },
-    });
-
     let board: JSX.Element[] = [];
+
+    const [piecePosition, setPiecePosition] = useState(initialPositions);
 
     for (let i = 0; i < verticalAxis.length; i++) {
         for (let j = 0; j < horizontalAxis.length; j++) {
