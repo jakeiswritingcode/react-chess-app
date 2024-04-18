18cdb88b1f82195e5627c08f512affe7dc680386
Jake Osborne <jake@jakeiswritingcode.com>
2024-04-18 13:35:11 -0500

    add not found page to router

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChessGamePage from './pages/ChessGamePage';
 import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFountPage'
 
 function App() {
   return (
     <BrowserRouter>
-    <div id="app">
-      <Routes>
-        <Route path="/" element={<ChessGamePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </div>
-  </BrowserRouter>    
+      <div id="app">
+        <Routes>
+          <Route path="/" element={<ChessGamePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NotFountPage.tsx
@@ -0,0 +1,10 @@
+// import { NavBar } from '../components/NavBar';
+// import { NotFoundBox } from '../components/NotFoundBox';
+
+function NotFoundPage() {
+    return (
+        <div /> // TODO: add components
+    );
+  }
+  
+  export default NotFoundPage;
