285009788bed52f98728f78f5df7aaafdcfef2cc
Jake Osborne <jake@jakeiswritingcode.com>
2024-04-26 19:10:00 -0500

    barrel server  updates

--- src/gameUpdates/index.ts
+++ src/gameUpdates/index.ts
@@ -1,1 +1,6 @@
 export type { GameUpdate } from './GameUpdate';
+export type { MoveUpdate } from './updates/MoveUpdate'
+export type { ChatUpdate } from './updates/ChatUpdate'
+export type { OfferDrawUpdate } from './updates/OfferDrawUpdate'
+export type { AcceptDrawUpdate } from './updates/AcceptDrawUpdate'
+export type { ForfeitUpdate } from './updates/ForfeitUpdate'
